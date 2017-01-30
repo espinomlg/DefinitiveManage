55ad5aa702b11bb441d736164f8d8599ee893083
Jose Gallardo <jose.gallardo994@gmail.com>
2017-01-30 12:14:05 +0100

    ManageProduct cursors finished.

---
+++ app/src/main/java/com/jsw/MngProductDatabase/Cursors/PharmacyCursorLoader.java
@@ -0,0 +1,47 @@
+package com.jsw.MngProductDatabase.Cursors;
+
+/*
+ * Copyright (c) 2017 José Luis del Pino Gallardo.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  jose.gallardo994@gmail.com
+ */
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v4.content.CursorLoader;
+
+import com.jsw.MngProductDatabase.database.DatabaseManager;
+
+/**
+ * Created by usuario on 30/01/17.
+ */
+
+public class PharmacyCursorLoader extends CursorLoader{
+
+    /**
+     * Creates an empty unspecified CursorLoader.  You must follow this with
+     * calls to {@link #setUri(Uri)}, {@link #setSelection(String)}, etc
+     * to specify the query to perform.
+     *
+     * @param context
+     */
+    public PharmacyCursorLoader(Context context) {
+        super(context);
+    }
+
+    @Override
+    public Cursor loadInBackground() {
+        return DatabaseManager.getInstance().getAllPharmacies();
+    }
+}
---
+++ app/src/main/java/com/jsw/MngProductDatabase/interfaces/IPharmacyPresenter.java
@@ -0,0 +1,40 @@
+package com.jsw.MngProductDatabase.interfaces;
+
+/*
+ * Copyright (c) 2017 José Luis del Pino Gallardo.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  jose.gallardo994@gmail.com
+ */
+
+import android.app.ProgressDialog;
+
+import com.jsw.MngProductDatabase.Model.Product;
+
+import java.util.List;
+
+/**
+ * Created by usuario on 30/01/17.
+ */
+
+public interface IPharmacyPresenter {
+
+    void getAllPharmacies();
+
+    interface View{
+        void showPharmacy(List<Product> products);
+        void showEmptyState(boolean show);
+        void showMessage(String message, Product product);
+        ProgressDialog getProgressDialog();
+    }
+}
